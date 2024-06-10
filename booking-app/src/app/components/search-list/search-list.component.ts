34a797addf0e2245ad9e2e1afb25199cdb793cd7
sona2711 <sonamkrtchyan2711@gmail.com>
2024-06-11 01:08:41 +0400

    Added checkboxes

--- booking-app/src/app/components/search-list/search-list.component.ts
+++ booking-app/src/app/components/search-list/search-list.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {JsonPipe} from '@angular/common';
+// import {JsonPipe} from '@angular/common';
 import { ProductDataService } from '../../services/product-data.service';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
@@ -10,8 +10,56 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   styleUrl: './search-list.component.scss',
 })
 export class SearchListComponent implements OnInit{
-constructor(productList: ProductDataService,) {}
+constructor(
+  productList: ProductDataService,
+   chechbox: MatCheckboxModule,
+  //  JsonPipe: JsonPipe,
+   private _formBuilder: FormBuilder,
+   formsModule: FormsModule,
+   reactiveFormsModule: ReactiveFormsModule
+
+  ) {}
 searchList = []
+popularFilters = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+facilities = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+propertyType = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+propertyRating = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+reviewScore = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+distanceFrom = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+funThings = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
+roomAccessibility = this._formBuilder.group({
+  stars: false,
+  hotels: false,
+  freeWifi: false,
+});
 
 ngOnInit(): void {
   
